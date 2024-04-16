694ef348cd98a6ece10cd8ba9eb3efdeebc5894a
9Xxi8Q4f <github.mutation748@slmail.me>
2024-04-16 16:07:46 +0300

    SAC init

--- src/agents/sac.py
+++ src/agents/sac.py
@@ -12,13 +12,105 @@ import time
 import tensorflow_probability as tfp
 import sys
 import random as rndm
+import keras
 
 tf.random.set_seed(43)
 
+class CriticNetwork(keras.Model):
+    def __init__(self, n_actions, fc1_dims=128, fc2_dims=128,
+            name='critic', chkpt_dir='tmp/sac'):
+        super(CriticNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
 
+        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc2 = Dense(self.fc2_dims, activation='relu')
+        self.q = Dense(1, activation=None)
+
+    def call(self, state, action):
+        action_value = self.fc1(tf.concat([state, action], axis=1))
+        action_value = self.fc2(action_value)
+
+        q = self.q(action_value)
+
+        return q
+
+class ValueNetwork(keras.Model):
+    def __init__(self, fc1_dims=128, fc2_dims=128,
+            name='value', chkpt_dir='tmp/sac'):
+        super(ValueNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+
+        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc2 = Dense(fc2_dims, activation='relu')
+        self.v = Dense(1, activation=None)
+
+    def call(self, state):
+        state_value = self.fc1(state)
+        state_value = self.fc2(state_value)
+
+        v = self.v(state_value)
+
+        return v
+
+class ActorNetwork(keras.Model):
+
+    def __init__(self, max_action, fc1_dims=128,
+            fc2_dims=128, n_actions=2, name='actor', chkpt_dir='tmp/sac'):
+        super(ActorNetwork, self).__init__()
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.model_name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_sac')
+        self.max_action = max_action
+        self.noise = 1e-6
+
+        self.fc1 = Dense(self.fc1_dims, activation='relu')
+        self.fc2 = Dense(self.fc2_dims, activation='relu')
+        self.mu = Dense(self.n_actions, activation=None)
+        self.sigma = Dense(self.n_actions, activation=None)
+
+    def call(self, state):
+        prob = self.fc1(state)
+        prob = self.fc2(prob)
+
+        mu = self.mu(prob)
+        sigma = self.sigma(prob)
+        # might want to come back and change this, perhaps tf plays more nicely with
+        # a sigma of ~0
+        sigma = tf.clip_by_value(sigma, self.noise, 1)
+
+        return mu, sigma
+
+    def sample_normal(self, state, reparameterize=True):
+        mu, sigma = self.call(state)
+        probabilities = tfp.distributions.Normal(mu, sigma)
+
+        if reparameterize:
+            actions = probabilities.sample() # + something else if you want to implement
+        else:
+            actions = probabilities.sample()
+
+        action = tf.math.tanh(actions)*self.max_action
+        log_probs = probabilities.log_prob(actions)
+        log_probs -= tf.math.log(1 - tf.math.pow(action,2)+self.noise)
+        log_probs = tf.math.reduce_sum(log_probs, axis=1, keepdims=True)
+
+        return action, log_probs
+    
 class SAC:
     def __init__(self, state_size, action_size, alpha=0.0003, beta=0.001, hidden_size=512, temperature=0.03,
-                 gamma=0.99, tau=0.005, buffer_size=int(1e6), batch_size=256, reward_scale=1.0):
+                 gamma=0.99, tau=0.005, buffer_size=int(1e6), batch_size=256, reward_scale=1.0, model_name = "SAC_DEMO"):
         """
         Params
         ======
@@ -35,6 +127,8 @@ class SAC:
         self.state_size = state_size
         self.action_size = action_size
         
+        self.model_name = f"{model_name}_{int(time.time())}"
+
         # Actor Network (w/ Target Network)
         self.actor_local = self._actor_network()
         self.actor_target = self._actor_network()
@@ -46,7 +140,6 @@ class SAC:
         self.critic_optimizer = Adam(lr=beta)
         
         self.hidden_size = hidden_size
-        self.reward_scale = reward_scale
         self.tempereture = temperature
 
         # Noise process
@@ -60,86 +153,56 @@ class SAC:
         self.gamma = gamma
         self.tau = tau
         
-        # Set up the optimizer
-        self.critic_criterion = tf.keras.losses.MeanSquaredError()
-        self.actor_criterion = tf.keras.losses.MeanSquaredError()
+        self.actor = ActorNetwork(max_action=1.0, fc1_dims=hidden_size, fc2_dims=hidden_size,
+                                  n_actions=action_size, name='actor')
+        self.critic_1 = CriticNetwork(n_actions=action_size, fc1_dims=hidden_size,
+                                      fc2_dims=hidden_size, name='critic_1')
+        self.critic_2 = CriticNetwork(n_actions=action_size, fc1_dims=hidden_size,
+                                      fc2_dims=hidden_size, name='critic_2')
+        self.value = ValueNetwork(fc1_dims=hidden_size, fc2_dims=hidden_size, name='value')
+        self.target_value = ValueNetwork(fc1_dims=hidden_size, fc2_dims=hidden_size, name='target_value')
+
+        self.actor.compile(optimizer=self.actor_optimizer)
+        self.critic_1.compile(optimizer=self.critic_optimizer)
+        self.critic_2.compile(optimizer=self.critic_optimizer)
+        self.value.compile(optimizer=self.critic_optimizer)
+        self.target_value.compile(optimizer=self.critic_optimizer)
+        self.scale = reward_scale
+
+        self.update_network_parameters(tau=1)
         
-        # Set up the epsilon greedy policy
-        self.epsilon = 1.0
-        self.epsilon_decay = 0.9999
-        self.min_epsilon = 0.01
-
-    def OUNoise(self, action):
-        pass
-
-    def _actor_network(self):
-        """
-        Creates the actor network for the SAC agent
-        """
-        network = Sequential([
-            Dense(64, input_dim=self.state_size, activation='relu'),
-            Dense(32, activation='relu'),
-            Dense(self.action_size, activation='sigmoid')
-        ])
-        network.compile(optimizer=self.actor_optimizer, loss=self.actor_criterion)
-        return network
-
-    def _critic_network(self):
-        """
-        Creates the critic network for the SAC agent
-        """
-        network = Sequential([
-            Dense(64, input_dim=self.state_size + self.action_size, activation='relu'),
-            Dense(32, activation='relu'),
-            Dense(1)
-        ])
-        network.compile(optimizer=self.critic_optimizer, loss=self.critic_criterion)
-        return network
+        self.tensorboard = ModifiedTensorBoard(log_dir=f"logs/sac/{self.model_name}")
         
     def choose_action(self, observation):
         """
         Chooses the next action based on the current observation
         """
-        self.actor_local.reset_states()
-        state = np.reshape(observation, [1, self.state_size])
-        action = self.actor_local.predict(state)[0]
-        # Add noise to the action
-        action = np.clip(action + self.noise.sample(), -1.0, 1.0)
-        self.epsilon *= self.epsilon_decay
-        self.epsilon = max(self.min_epsilon, self.epsilon)
-        return action
+
+        #* A noise added in sample_normal function
+        state = tf.convert_to_tensor([observation])
+        actions, _ = self.actor.sample_normal(state, reparameterize=False)
+        
+        return actions[0]
 
     def remember(self, state, action, reward, next_state, done):
         """
         Stores the next experience in the replay memory
         """
-        self.memory.add(state, action, reward, next_state, done)
+        self.memory.store_transition(state, action, reward, next_state, done)
     
-    def update_network_parameters(self, batch_size):
+    def update_network_parameters(self, tau=None):
         """
         Updates the parameters of the critic and actor networks given a mini-batch of experiences
         """
-        experiences = self.memory.sample(batch_size)
-        states, actions, rewards, next_states, dones = experiences
-        
-        # Update the critic network using the target actor network
-        with tf.GradientTape() as tape:
-            # Get the predicted next-state actions from the target actor network
-            next_actions = self.actor_target(next_states)
-            # Compute the target Q values using the target critic network
-            Q_targets_next = self.critic_target([next_states, next_actions])
-            # Compute the expected Q values
-            Q_targets = rewards + (self.gamma * Q_targets_next * (1 - dones))
-            # Compute the critic loss
-            Q_expected = self.critic_local([states, actions])
-            critic_loss = self.critic_criterion(Q_expected, Q_targets)
-        
-        # Backpropagate the critic loss
-        critic_grad = tape.gradient(critic_loss, self.critic_local.trainable_variables)
-        self.critic_optimizer.apply_gradients(zip(critic_grad, self.critic_local.trainable_variables))
-        
-        # Update the actor network
-        with tf.GradientTape() as tape:
-            # Compute the actor
-            pass
+
+        if tau is None:
+            tau = self.tau
+
+        weights = []
+        targets = self.target_value.weights
+        for i, weight in enumerate(self.value.weights):
+            weights.append(weight * tau + targets[i]*(1-tau))
+
+        self.target_value.set_weights(weights)
+
 
